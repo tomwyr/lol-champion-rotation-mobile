e30b9862b0a4ecac5395b4bbd82bf6f20075cf8e
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-02-03 22:53:37 +0100

    Fix status bar icon colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'common/app_config.dart';
 import 'dependencies.dart';
@@ -9,19 +8,10 @@ import 'ui/app.dart';
 void main() async {
   await initializeConfigs();
   setUpDependencies();
-  configureSystemUI();
   runApp(const App());
 }
 
 Future<void> initializeConfigs() async {
   await AppConfig.initEnv();
   await Firebase.initializeApp();
-}
-
-void configureSystemUI() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.transparent,
-  ));
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 }
--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:toastification/toastification.dart';
 
 import '../core/stores/app.dart';
@@ -19,11 +20,14 @@ class App extends StatelessWidget {
         child: NotificationsInitializer(
           child: ValueListenableBuilder(
             valueListenable: store.themeMode,
-            builder: (context, value, _) => MaterialApp(
+            builder: (context, value, _) => AppBrightnessStyle(
               themeMode: value,
-              theme: AppMaterialTheme.light(),
-              darkTheme: AppMaterialTheme.dark(),
-              home: const HomePage(),
+              child: MaterialApp(
+                themeMode: value,
+                theme: AppMaterialTheme.light(),
+                darkTheme: AppMaterialTheme.dark(),
+                home: const HomePage(),
+              ),
             ),
           ),
         ),
@@ -105,6 +109,39 @@ class AppNotifications extends StatelessWidget {
       showProgressBar: false,
       closeButtonShowType: CloseButtonShowType.none,
       description: Text(message),
+    );
+  }
+}
+
+class AppBrightnessStyle extends StatelessWidget {
+  const AppBrightnessStyle({
+    super.key,
+    required this.themeMode,
+    required this.child,
+  });
+
+  final ThemeMode themeMode;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = switch (themeMode) {
+      ThemeMode.system => MediaQuery.of(context).platformBrightness,
+      ThemeMode.light => Brightness.light,
+      ThemeMode.dark => Brightness.dark,
+    };
+
+    final (background, foreground) = switch (brightness) {
+      Brightness.light => (Brightness.light, Brightness.dark),
+      Brightness.dark => (Brightness.dark, Brightness.light),
+    };
+
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+        statusBarBrightness: background,
+        statusBarIconBrightness: foreground,
+      ),
+      child: child,
     );
   }
 }
