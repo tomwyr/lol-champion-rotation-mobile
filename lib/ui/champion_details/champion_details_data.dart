49916f99d9bc20c4ba176dec6ae954b0c46d2e6e
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-02-26 01:00:05 +0100

    Use collapsing toolbar in champion details

--- lib/ui/champion_details/champion_details_data.dart
+++ lib/ui/champion_details/champion_details_data.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 
 import '../../core/model/champion.dart';
 import '../common/theme.dart';
+import '../common/widgets/sliver_collapsing_toolbar.dart';
 import '../common/widgets/utils/assets.dart';
 
 class ChampionDetailsData extends StatelessWidget {
@@ -17,115 +18,130 @@ class ChampionDetailsData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(4),
-                      child: SizedBox.square(
-                        dimension: 96,
-                        child: CachedNetworkImage(imageUrl: details.imageUrl),
-                      ),
-                    ),
-                    const SizedBox(width: 16),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            details.name,
-                            style: Theme.of(context).textTheme.headlineMedium,
-                          ),
-                          Text(
-                            details.title,
-                            style: const TextStyle(
-                              height: 0.75,
-                              fontWeight: FontWeight.w300,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                          if (_subtitleAvailability() case var availability?) ...[
-                            const SizedBox(height: 4),
-                            DefaultTextStyle(
-                              style: Theme.of(context).textTheme.bodyLarge!,
-                              child: _buildDescription(context, availability),
-                            ),
-                          ],
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 12),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 8,
-                  children: [
-                    Text(
-                      'Rotations',
-                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                            fontWeight: FontWeight.w300,
-                          ),
-                    ),
-                    ...[
-                      for (var availability in details.rotationsAvailability)
-                        Row(
-                          children: [
-                            SizedBox.square(
-                              dimension: 32,
-                              child: Image.asset(availability.rotationType.imageAsset),
-                            ),
-                            const SizedBox(width: 12),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  availability.rotationType.displayName,
-                                  style: const TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                _buildDescription(context, availability),
-                              ],
-                            ),
-                          ],
-                        ),
-                    ],
-                  ],
-                ),
-              ],
-            ),
+        child: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            _AppBar(details: details),
+          ],
+          body: ListView(
+            padding: const EdgeInsets.fromLTRB(16, 12, 16, 0),
+            children: [
+              _RotationsSection(details: details),
+            ],
           ),
         ),
       ),
     );
   }
+}
 
-  Widget _buildDescription(BuildContext context, ChampionDetailsAvailability availability) {
-    if (availability.current) {
-      return Text(
-        'Available',
-        style: TextStyle(color: context.appTheme.availableColor),
-      );
+class _AppBar extends StatelessWidget {
+  const _AppBar({required this.details});
+
+  final ChampionDetails details;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverCollapsingAppBar(
+      collapsedHeight: 56,
+      expandedHeight: 152,
+      builder: (expansion) {
+        final verticalPadding = 10 * (1 - expansion);
+        final imageMargin = 48 * (1 - expansion);
+        final nameMargin = 8 + 8 * expansion;
+
+        return Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: verticalPadding,
+            horizontal: 16,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(width: imageMargin),
+              _image(),
+              SizedBox(width: nameMargin),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _name(context, expansion),
+                    Expanded(
+                      child: _subtitle(context, expansion),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _image() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4),
+      child: CachedNetworkImage(imageUrl: details.imageUrl),
+    );
+  }
+
+  Widget _name(BuildContext context, double expansion) {
+    final collapsedStyle = Theme.of(context).textTheme.headlineSmall;
+    final expandedStyle = Theme.of(context).textTheme.headlineMedium;
+    final collapsedSize = collapsedStyle?.fontSize;
+    final expandedSize = expandedStyle?.fontSize;
+
+    double? fontSize;
+    if (collapsedSize != null && expandedSize != null) {
+      fontSize = collapsedSize + (expandedSize - collapsedSize) * expansion;
     }
-    if (availability.lastAvailable case var lastAvailable?) {
-      final formattedDate = DateFormat('MMM dd').format(lastAvailable);
-      return Text('Last available $formattedDate');
-    }
+
     return Text(
-      'Unavailable',
-      style: TextStyle(color: context.appTheme.unavailableColor),
+      details.name,
+      style: expandedStyle?.copyWith(fontSize: fontSize),
+    );
+  }
+
+  Widget _subtitle(BuildContext context, double expansion) {
+    return Opacity(
+      opacity: expansion,
+      child: OverflowBox(
+        alignment: Alignment.topLeft,
+        maxHeight: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _title(),
+            if (_subtitleAvailability() case var availability?) ...[
+              const SizedBox(height: 4),
+              _availabilityDescription(
+                context,
+                availability,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return Text(
+      details.title,
+      style: const TextStyle(
+        height: 0.75,
+        fontWeight: FontWeight.w300,
+        fontStyle: FontStyle.italic,
+      ),
     );
   }
 
   ChampionDetailsAvailability? _subtitleAvailability() {
-    final sorted = details.rotationsAvailability.toList();
-    sorted.sort((lhs, rhs) {
+    final values = details.rotationsAvailability.toList();
+    values.sort((lhs, rhs) {
       if (lhs.current && !rhs.current) return -1;
       if (!lhs.current && rhs.current) return 1;
 
@@ -134,6 +150,82 @@ class ChampionDetailsData extends StatelessWidget {
       if (lhs.lastAvailable == null) return 1;
       return -lhs.lastAvailable!.compareTo(rhs.lastAvailable!);
     });
-    return sorted.firstOrNull;
+    return values.firstOrNull;
   }
+}
+
+class _RotationsSection extends StatelessWidget {
+  const _RotationsSection({required this.details});
+
+  final ChampionDetails details;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        _header(context),
+        for (var availability in details.rotationsAvailability)
+          _rotationAvailability(context, availability)
+      ],
+    );
+  }
+
+  Widget _header(BuildContext context) {
+    return Text(
+      'Rotations',
+      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+            fontWeight: FontWeight.w300,
+          ),
+    );
+  }
+
+  Widget _rotationAvailability(BuildContext context, ChampionDetailsAvailability availability) {
+    return Row(
+      children: [
+        SizedBox.square(
+          dimension: 32,
+          child: Image.asset(availability.rotationType.imageAsset),
+        ),
+        const SizedBox(width: 12),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              availability.rotationType.displayName,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            _availabilityDescription(context, availability),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+Widget _availabilityDescription(
+  BuildContext context,
+  ChampionDetailsAvailability availability, {
+  TextStyle? style,
+}) {
+  style ??= const TextStyle();
+
+  if (availability.current) {
+    return Text(
+      'Available',
+      style: style.copyWith(color: context.appTheme.availableColor),
+    );
+  }
+  if (availability.lastAvailable case var lastAvailable?) {
+    final formattedDate = DateFormat('MMM dd').format(lastAvailable);
+    return Text(
+      'Last available $formattedDate',
+      style: style,
+    );
+  }
+  return Text(
+    'Unavailable',
+    style: style.copyWith(color: context.appTheme.unavailableColor),
+  );
 }
---
+++ lib/ui/common/widgets/sliver_collapsing_toolbar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SliverCollapsingAppBar extends StatelessWidget {
+  const SliverCollapsingAppBar({
+    super.key,
+    required this.collapsedHeight,
+    required this.expandedHeight,
+    required this.builder,
+  });
+
+  final double collapsedHeight;
+  final double expandedHeight;
+  final Widget Function(double expansion) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      toolbarHeight: collapsedHeight,
+      collapsedHeight: collapsedHeight,
+      expandedHeight: expandedHeight,
+      pinned: true,
+      flexibleSpace: LayoutBuilder(
+        builder: (context, constraints) {
+          final expansion =
+              (constraints.maxHeight - collapsedHeight) / (expandedHeight - collapsedHeight);
+
+          return Padding(
+            padding: EdgeInsets.only(top: collapsedHeight * expansion),
+            child: SizedBox(
+              height: expandedHeight - collapsedHeight,
+              child: builder(expansion),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
